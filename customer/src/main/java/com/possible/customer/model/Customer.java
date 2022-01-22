74bf29d9528a564fd434006237390ab4a33fed75
olayode <aolayode@getcarbon.co>
2022-01-22 14:46:06 +0100

    complete customer service create operation

--- customer/src/main/java/com/possible/customer/model/Customer.java
+++ customer/src/main/java/com/possible/customer/model/Customer.java
@@ -1,11 +1,21 @@
 package com.possible.customer.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+    @Id
+    @SequenceGenerator(name = "customer_id_seq", sequenceName = "customer_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_seq")
     private Integer id;
     private String firstName;
     private String lastName;
--- customer/src/main/java/com/possible/customer/services/CustomerService.java
+++ customer/src/main/java/com/possible/customer/services/CustomerService.java
@@ -1,11 +1,12 @@
 package com.possible.customer.services;
 
+import com.possible.customer.CustomerRepository;
 import com.possible.customer.model.Customer;
 import com.possible.customer.model.dto.CustomerRegistrationRequest;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -13,5 +14,7 @@ public record CustomerService() {
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+
+        customerRepository.save(customer);
     }
 }
